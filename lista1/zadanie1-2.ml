e6941a9eb596b3ac6e0dc8de24debb8e536f0e55
cytrynka37 <cytrynka337@gmail.com>
2024-10-14 22:18:10 +0200

    .

--- lista1/zadanie1-2.ml
+++ lista1/zadanie1-2.ml
@@ -1,4 +1,4 @@
-(*zadanie1*)
+(*zadanie 1*)
 let z1 = fun x -> x
 let z2 = fun x -> x * 1
 let z3 = fun a b c -> a (b c)
@@ -6,5 +6,5 @@ let z4 = fun a b -> a
 let z5 a b = if true then a else b
 let z6 = raise Not_found
 
-(*zadanie2*)
+(*zadanie 2*)
 let rec f x = f x
--- lista1/zadanie3-5.ml
+++ lista1/zadanie3-5.ml
@@ -1,4 +1,4 @@
-(*zadanie3*)
+(*zadanie 3*)
 let hd (s : int -> 't) = s 0
 
 let tl (s : int -> 't) = fun n -> s (n + 1)
@@ -13,14 +13,19 @@ let replace (s : int -> 't) n a = fun i -> if i = n then a else s i
 
 let take_every (s : int -> 't) n = fun i -> s (n * i)
 
-
 let s1 = fun n -> n
 
 let s2 = fun n -> n * n
 
-(*zadanie4*)
+(*zadanie 4*)
 let scan (f : 'a -> 'b -> 'a) (a : 'a) (s : int -> 'b) =
 let rec helper = fun i -> 
     if i == 0 then f a (s 0)
     else f (helper (i - 1)) (s i)
 in helper 
+
+(*zadanie 5*)
+let tabulate (s : int -> 't) ?(a=0) b =
+    let rec helper acc x =
+        if x - a < 0 then acc else helper ((s x) :: acc) (x - 1)
+    in helper [] b
